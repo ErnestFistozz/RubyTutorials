1038940f19a2275e3c61687600cc1a9c59e82493
NB320756 <ErnestM@nedbank.co.za>
2020-07-08 21:32:00 +0200

    types of variables in ruby and how to declare

--- tutorial1_class_objects.rb
+++ tutorial1_class_objects.rb
@@ -4,3 +4,41 @@
 class Custumer
 
 end
+
+=begin
+Types of variables in Ruby
+=end
+
+#Local variables
+#available within the 'method' only
+#Examples
+# name= 'Ernes'
+# _age = 4
+
+
+#Instance variables
+#available across instance methods or objects
+#Examples
+# @name
+# @age
+class Student
+	@student_number
+end
+
+#Class Variable
+#available across all objects
+# Examples
+# @@name
+#@@age
+
+class Person
+	@@no_of_people = 0
+end
+
+
+#Global variables
+#available across the whol script
+#Examples
+# $name
+
+$file_name = 'ruby'
