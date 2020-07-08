c585348717291b4b847c1e148be7fb3570deb631
NB320756 <ErnestM@nedbank.co.za>
2020-07-08 21:03:34 +0200

    adding ruby initial file

---
+++ ruby.rb
@@ -0,0 +1,19 @@
+variable = 2
+
+#This is a comment
+a = 1
+b = 'string'
+c = Object.new
+
+bacon_type = 'crisp'
+2.times do
+	puts bacon_type
+	temperature = 300
+end
+hour = 15
+
+if hour% 3 ==0
+	puts 'Its BACON time'
+end
+
+puts Math.sqrt(19)
